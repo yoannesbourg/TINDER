fd699e791f707cc381679fab5957f5a4d804db96
Yoann <yoannpascalbouix@gmail.com>
2021-03-26 20:27:48 +0100

    Post a user

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const pool = require("./db")
 
 
 //middleware
 app.use(cors())
-app.use(express.json())
+app.use(express.json()) //req.body
+
+//ROUTES
+
+//Create user
+app.post("/users", async (req, res) => {
+    try {
+        console.log(req.body)
+    } catch (err) {
+        console.error(err)
+    }
+})
 
 app.listen(5000, () => {
     console.log("server has started on port 5000")
