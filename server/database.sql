c4fe9f19b8092f24d23ec1e2051a2b1ec2680025
Yoann <yoannpascalbouix@gmail.com>
2021-03-29 12:05:17 +0200

    Database works (finally)

--- server/database.sql
+++ server/database.sql
@@ -3,7 +3,9 @@ CREATE DATABASE tinder;
 CREATE TABLE users (
  id SERIAL PRIMARY KEY,
  name varchar(255) NOT NULL,
- photo varchar(255) NOT NULL,
+ photo varchar(255),
  birthdate integer NOT NULL,
  description varchar(255) NOT NULL
 );
+
+
