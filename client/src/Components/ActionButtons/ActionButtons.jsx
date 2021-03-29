6ea196bd9ee62ca6071167f5f501d9ac37085d2a
Yoann <yoannpascalbouix@gmail.com>
2021-03-29 19:50:03 +0200

    Design structure

--- client/src/Components/ActionButtons/ActionButtons.jsx
+++ client/src/Components/ActionButtons/ActionButtons.jsx
@@ -3,7 +3,7 @@ import '../../App.css';
 const ActionButtons = () => {
   return (
     <div>
-      Card
+      Buttons
     </div>
   );
 }
--- client/src/Components/Navigation/Navigation.jsx
+++ client/src/Components/Navigation/Navigation.jsx
@@ -1,11 +1,14 @@
 import '../../App.css';
 import { AiOutlineUser } from "react-icons/ai";
+import { AiFillFire } from "react-icons/ai";
+import { AiFillMessage } from "react-icons/ai";
 
 const Navigation = () => {
   return (
     <div>
       <AiOutlineUser />
-      Navigation
+      <AiFillFire />
+      <AiFillMessage />
     </div>
   );
 }
