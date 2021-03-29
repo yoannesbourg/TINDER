8422f06e60bbdbea3c2f60b0c0083b026fd22a23
Yoann <yoannpascalbouix@gmail.com>
2021-03-29 20:03:36 +0200

    Profile Card Design

--- client/src/Components/ProfileCard/ProfileCard.jsx
+++ client/src/Components/ProfileCard/ProfileCard.jsx
@@ -2,8 +2,8 @@ import '../../App.css';
 
 const ProfileCard = () => {
   return (
-    <div>
-      Card
+    <div className="profile-card">
+      <h2 className="user-name">Name, 25</h2>
     </div>
   );
 }
