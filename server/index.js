d21e4485ecab77dd7dc9119276a4a0ce0c47ae3c
Yoann <yoannpascalbouix@gmail.com>
2021-03-29 15:27:35 +0200

    Post a user

--- server/index.js
+++ server/index.js
@@ -14,37 +14,46 @@ app.use(bodyParser.json())
 //Create user
 app.post("/users", async (req, res) => {
     try {
-        const { description } = req.body
-        // const newDescription = await pool.query("INSERT INTO users (description) VALUES($1) RETURNING *", [description])
-        // res.json(newDescription.rows[0])
-        console.log(req)
+        const body = req.body
+        const newDescription = await pool.query("INSERT INTO users (name, photo, birthdate, description) VALUES($1, $2, $3, $4) RETURNING *", [body.name, body.photo, body.birthdate, body.description])
+        res.json(newDescription.rows[0])
     } catch (err) {
         console.error(err)
     }
 })
 
-//Get all user 
+// //Get all user 
 app.get("/users", async (req, res) => {
     try {
-        const { description } = req.body
+        const { description } = req.query
         const allUsers = await pool.query("SELECT * FROM users")
         res.json(allUsers.rows)
-        console.log(description)
     } catch (error) {
         console.error(error.message)
     }
 })
 
-//Delete user
-app.delete("/users/:id", async (req, res) => {
+//Get a single user
+app.get("/users/:id", async (req, res) => {
     try {
         const { id } = req.params
-        const deleteTodo = await pool.query("DELETE FROM users WHERE id = $1", [id])
-        res.json("User was deleted")
-    } catch (err) {
-        console.error(err)
+        const user = await pool.query("SELECT * FROM users WHERE id = $1", [id])
+        res.json(user.rows)
+    } catch (error) {
+        
     }
 })
+
+//Delete user
+// app.delete("/users/:id", async (req, res) => {
+//     try {
+//         const { id } = req.params
+//         const deleteTodo = await pool.query("DELETE FROM users WHERE id = $1", [id])
+//         res.json("User was deleted")
+//     } catch (err) {
+//         console.error(err)
+//     }
+// })
 
 
 app.listen(5000, () => {
