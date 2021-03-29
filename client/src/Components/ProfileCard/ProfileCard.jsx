5acdd81c6e2c5ae54b2d4d3941cc086df242565c
Yoann <yoannpascalbouix@gmail.com>
2021-03-29 20:11:40 +0200

    Fetch data for profile Card

--- client/src/Components/ProfileCard/ProfileCard.jsx
+++ client/src/Components/ProfileCard/ProfileCard.jsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from 'react';
 import '../../App.css';
 
 const ProfileCard = () => {
+  const [data, setData] = useState({})
+
+  const getData = async () => {
+    try {
+        const response = await fetch("http://localhost:5000/users/14")
+        const jsonData = await response.json()
+        setData(jsonData)
+        console.log(jsonData)
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+ useEffect(() => {
+   getData()
+ }, [])
+
   return (
     <div className="profile-card">
       <h2 className="user-name">Name, 25</h2>
