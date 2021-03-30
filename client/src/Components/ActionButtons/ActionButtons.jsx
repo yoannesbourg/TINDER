4550f0e136e6389a7123898fdbd3ac37f16f6e35
Yoann <yoannpascalbouix@gmail.com>
2021-03-30 16:59:56 +0200

    Action buttons layout

--- client/src/Components/ActionButtons/ActionButtons.jsx
+++ client/src/Components/ActionButtons/ActionButtons.jsx
@@ -1,10 +1,62 @@
-import '../../App.css';
+import '../../App.css'
+import styled from "styled-components"
+import { AiOutlineClose } from "react-icons/ai"
+import { BsHeartFill } from "react-icons/bs"
+import { BsStarFill } from "react-icons/bs"
+import { HiX } from "react-icons/hi";
+
+
+const ActionContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`
+
+const ActionButton = styled.button`
+ border: none;
+ width: 60px;
+ height: 60px;
+ margin: 16px 0;
+ border-radius: 50%;
+ font-size: 25px;
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ box-shadow: 0 0 6px 2px #a0a0a0;
+ outline: none;
+`
+
+const Dislike = styled(ActionButton)`
+  font-weight: bold;
+`
+
+const Superlike = styled(ActionButton)`
+  width: 50px;
+  height: 50px;
+  margin-right: 16px;
+  margin-left: 16px;
+`
+
+const Like = styled(ActionButton)`
+
+`
 
 const ActionButtons = () => {
   return (
-    <div>
-      Buttons
-    </div>
+    <ActionContainer>
+      <Dislike>
+        <HiX/>
+      </Dislike>
+      
+      <Superlike>
+        <BsStarFill/>
+      </Superlike>
+      
+      <Like>
+        <BsHeartFill/>
+      </Like>
+    </ActionContainer>
   );
 }
 
