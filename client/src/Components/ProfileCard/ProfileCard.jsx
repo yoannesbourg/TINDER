c98ff92dc86d0c8aebf738b714ebc39d2d98f878
Yoann <yoannpascalbouix@gmail.com>
2021-03-30 15:43:05 +0200

    Styled CardTitle

--- client/src/Components/ProfileCard/ProfileCard.jsx
+++ client/src/Components/ProfileCard/ProfileCard.jsx
@@ -1,10 +1,14 @@
 import styled from "styled-components"
 
+const CardTitle = styled.h2`
+  color: var(--white);
+  text-shadow: 2px 2px 2px #5e5e5e;
+`
+
 const ProfileCard = ({userData}) => {
  
  const Card = styled.div`
     background-color: var(--PastelRed);
-    color: var(--white);
     width: 100%;
     height: 350px;
     margin-top: 16px;
@@ -15,7 +19,7 @@ const ProfileCard = ({userData}) => {
  `
   return (
     <Card>
-      <h2>{userData.name}, 25</h2>
+      <CardTitle>{userData.name}, 25</CardTitle>
     </Card>
   );
 }
