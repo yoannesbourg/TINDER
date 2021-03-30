6bf216baaf1d77cbf254b0379c3f79d80a274412
Yoann <yoannpascalbouix@gmail.com>
2021-03-30 13:25:27 +0200

    Using styling components on ProfileCard

--- client/src/Components/ProfileCard/ProfileCard.jsx
+++ client/src/Components/ProfileCard/ProfileCard.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import '../../App.css';
+import styled from "styled-components"
 
 const ProfileCard = () => {
   const [data, setData] = useState({})
@@ -9,20 +10,33 @@ const ProfileCard = () => {
         const response = await fetch("http://localhost:5000/users/14")
         const jsonData = await response.json()
         setData(jsonData)
-        console.log(jsonData)
+        // console.log(jsonData[0].photo)
     } catch (err) {
         console.error(err)
     }
-}
+  } 
 
- useEffect(() => {
-   getData()
- }, [])
+  useEffect(() => {
+    getData()
+  }, [])
+  console.log(data)
+ 
+ const Card = styled.div`
+    background-color: var(--PastelRed);
+    width: 100%;
+    height: 350px;
+    margin-top: 16px;
+    display: flex;
+    align-items: flex-end;
+    padding-left: 16px;
+    
+ `
+ 
 
   return (
-    <div className="profile-card">
+    <Card>
       <h2 className="user-name">Name, 25</h2>
-    </div>
+    </Card>
   );
 }
 
