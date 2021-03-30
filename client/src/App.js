3519f0b0463aba24433b25556ec7ed26af415776
Yoann <yoannpascalbouix@gmail.com>
2021-03-30 16:59:32 +0200

    App to styled components

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,21 @@ import './App.css';
 import Navigation from './Components/Navigation/Navigation'
 import ProfileCard from './Components/ProfileCard/ProfileCard'
 import ActionButtons from './Components/ActionButtons/ActionButtons'
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
+import styled from "styled-components"
+
+const AppMain = styled.main`
+  background-color: white;
+  min-width: 500px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: calc(10px + 2vmin);
+  color: var(--GreyText);
+  box-shadow: 0 0 6px 2px #a0a0a0;
+  border-radius: 20px;
+`
+
 const App = () => {
 
   const [user, setUser] = useState({})
@@ -26,13 +40,13 @@ const App = () => {
 
   return (
     <div className="App">
-      <header className="App-header">
+      <AppMain>
         <Navigation />
 
         {!isLoading? <p>Loading</p> : <ProfileCard userData={user[0]}/>}
         
         <ActionButtons />
-      </header>
+      </AppMain>
     </div>
   );
 }
