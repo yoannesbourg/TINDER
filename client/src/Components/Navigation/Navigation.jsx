e2d66aacba9db8c757182efb680d3fc313da55be
Yoann <yoannpascalbouix@gmail.com>
2021-03-31 19:55:26 +0200

    Navigation block to styled components

--- client/src/Components/Navigation/Navigation.jsx
+++ client/src/Components/Navigation/Navigation.jsx
@@ -2,14 +2,23 @@ import '../../App.css';
 import { AiOutlineUser } from "react-icons/ai";
 import { AiFillFire } from "react-icons/ai";
 import { AiFillMessage } from "react-icons/ai";
+import styled from "styled-components"
+
+const NavigationBlock = styled.div`
+width: 100%;
+display: flex;
+justify-content: space-around;
+margin-top: 16px;
+`
+
 
 const Navigation = () => {
   return (
-    <div className="navigation">
+    <NavigationBlock>
       <AiOutlineUser />
       <AiFillFire />
       <AiFillMessage />
-    </div>
+    </NavigationBlock>
   );
 }
 
