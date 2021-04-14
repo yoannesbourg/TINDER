4329ffb43593c31f9aebb4bf3fac44f8dc957027
Yoann <yoannpascalbouix@gmail.com>
2021-04-14 12:42:32 +0200

    Styled components in a searate file

--- client/src/Components/ActionButtons/ActionButtons.jsx
+++ client/src/Components/ActionButtons/ActionButtons.jsx
@@ -4,49 +4,7 @@ import { BsHeartFill } from "react-icons/bs"
 import { BsStarFill } from "react-icons/bs"
 import { HiX } from "react-icons/hi"
 import { useEffect, useState } from 'react'
-
-const ActionContainer = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: center;
-  align-items: center;
-`
-
-const ActionButton = styled.button`
- border: none;
- width: 60px;
- height: 60px;
- margin: 32px 0;
- border-radius: 50%;
- font-size: 25px;
- display: flex;
- justify-content: center;
- align-items: center;
- box-shadow: 0 0 6px 2px #a0a0a0;
- outline: none;
- transition: ease-in-out .2s;
-
- &:hover {
-  opacity: .8;
- }
- &:active {
-transform: scale(.9);
- }
-`
-
-const Dislike = styled(ActionButton)`
-  font-weight: bold;
-`
-
-const Superlike = styled(ActionButton)`
-  width: 50px;
-  height: 50px;
-  margin-right: 16px;
-  margin-left: 16px;
-`
-
-const Like = styled(ActionButton)``
-
+import {ActionContainer, ActionButton, Dislike, Superlike, Like} from '../Styled-Components/StyledComponents'
 
 const ActionButtons = ({id, click, handleMatch}) => {
 
@@ -55,7 +13,6 @@ const ActionButtons = ({id, click, handleMatch}) => {
   const updateUser = async (e) => {
     e.preventDefault()
     try {
-  
       const response = await fetch(`http://localhost:5000/users/${id}`, {
         method: "PUT",
         headers: {"Content-Type": "application/json"}    
@@ -68,17 +25,16 @@ const ActionButtons = ({id, click, handleMatch}) => {
 
   const getMatch = async () => {
     try {
-      const response = await fetch(`http://localhost:5000/liked/${id}`)
-      const jsonData = await response.json()
-      setLikedYou(jsonData)
-  } catch (err) {
-      console.error(err)
-  }
+        const response = await fetch(`http://localhost:5000/liked/${id}`)
+        const jsonData = await response.json()
+        setLikedYou(jsonData)
+    } catch (err) {
+        console.error(err)
+    }
   }
 
   const handleLike = () => {
     handleMatch(likedYou)
-    
   }
 
   useEffect(() => {
